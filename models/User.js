a90b85fa997bb4e4a2f2561cd93b049d65f7e565
daregoodness1997 <daregoodness@gmail.com>
2022-06-16 09:57:00 +0100

    refactored error

--- models/User.js
+++ models/User.js
@@ -27,7 +27,7 @@ const UserSchema = new mongoose.Schema({
 
 UserSchema.pre('save', async function (next) {
   const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hashed(this.password, salt);
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
