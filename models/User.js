7f1fb35e4b38684b4f0b3ab881e45227afb9a50e
daregoodness1997 <daregoodness@gmail.com>
2022-06-16 09:44:13 +0100

    hashed password

--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minLength: 6,
-    maxLength: 12,
+    // maxLength: 12,
   },
 });
 
