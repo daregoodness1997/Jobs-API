66a75d4716623fd8257a47707607054b1ef4e073
daregoodness1997 <daregoodness@gmail.com>
2022-06-21 11:02:19 +0100

    not-found error handling, get single job

--- errors/index.js
+++ errors/index.js
@@ -1,5 +1,11 @@
 const CustomAPIError = require('./custom-error');
 const BadRequest = require('./bad-request');
 const UnauthenticatedError = require('./unauthenticated');
+const NotFoundError = require('./not-found');
 
-module.exports = { CustomAPIError, BadRequest, UnauthenticatedError };
+module.exports = {
+  CustomAPIError,
+  BadRequest,
+  UnauthenticatedError,
+  NotFoundError,
+};
