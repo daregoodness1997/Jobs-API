5d0bc1c037c7324d422d5a6adc2d0204a23ecfb7
daregoodness1997 <daregoodness@gmail.com>
2022-06-21 09:19:52 +0100

    auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const { UnauthenticatedError } = require('../errors');
+
+const auth = async (req, res, next) => {
+  // check authentication header
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    throw new UnauthenticatedError('Authentication invalid');
+  }
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    // const user = User.findOne(payload.id).select('-password');
+
+    // Attach the user to the job routes
+    req.user = { userId: payload.userId, name: payload.name };
+  } catch (err) {
+    throw new UnauthenticatedError('Authentication invalid');
+  }
+
+  next();
+};
+
+module.exports = auth;
