a43093bbad2e69d85247ca818b76bfdb0a1679a4
daregoodness1997 <daregoodness@gmail.com>
2022-06-22 14:44:45 +0100

    rate limiting and security

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages require
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 // connect DB to
@@ -15,7 +22,14 @@ const authMiddleware = require('./middleware/auth');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// rate limiter
+app.set('trust proxy', 1);
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
+
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 // extra packages
 
 // routes
